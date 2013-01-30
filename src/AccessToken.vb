8803ba1984f34453e8fd343c0e8fd5b3de85b0c6
aravindark <aravindark@aditi.com>
2013-01-30 12:01:58 +0530

    removing

--- src/AccessToken.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class AccessToken
-    Public Property expires_in
-    Public Property access_token
-    Public Property userid
-End Class
--- tools/Install.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$readmeFile = "http://www.aditicloud.com/nugets/gettingstarted"
-$DTE.ItemOperations.Navigate($readmeFile, [EnvDTE.vsNavigateOptions]::vsNavigateOptionsNewWindow)
